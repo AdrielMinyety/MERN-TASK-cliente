0cb6587637cc3d78a626c68ac5e2e64a9fc67d4f
unknown <AM3114779@AM3114779.c3edu.online>
2020-06-15 21:02:49 -0400

    Frontend repository

---
+++ precache-manifest.ded2d43d1ba4fea7e0a424d64309f941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503248525d79537a0d0d43e96d69995c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c9e7dfff4cbe5db244f",
+    "url": "./static/css/main.e75275a2.chunk.css"
+  },
+  {
+    "revision": "910e466541102d017040",
+    "url": "./static/js/2.1efeba3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1efeba3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9e7dfff4cbe5db244f",
+    "url": "./static/js/main.4a0bba9a.chunk.js"
+  },
+  {
+    "revision": "c96de6ec85a872d16b0b",
+    "url": "./static/js/runtime-main.a1a33307.js"
+  }
+]);
